e6b808a2eba81fffbfbef9b58032f54b45778e8b
Man 150 <man.davra@swiftrut.com>
2024-12-30 00:36:21 +0530

    server

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+module.exports = app;
