cb6a43cff5ec831c88fae0da5ec54aa36a308b4c
Man 150 <man.davra@swiftrut.com>
2024-12-31 22:50:39 +0530

    adddd

--- backend/routes/ride.routes.js
+++ backend/routes/ride.routes.js
@@ -9,7 +9,7 @@ router.post('/create',
     authMiddleware.authUser,
     body('pickup').isString().isLength({ min: 3 }).withMessage('Invalid pickup address'),
     body('destination').isString().isLength({ min: 3 }).withMessage('Invalid destination address'),
-    body('vehicleType').isString().isIn([ 'auto', 'car', 'moto' ]).withMessage('Invalid vehicle type'),
+    body('/').isString().isIn([ 'auto', 'car', 'moto' ]).withMessage('Invalid vehicle type'),
     rideController.createRide
 )
 
